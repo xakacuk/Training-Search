9f36350791ce594006de65978ef4658feaffa1cd
Matthijs <matthijs@Matthijss-MacBook-Pro.local>
2016-08-09 11:32:40 +0200

    Download artwork images using a UIImageView extension

--- StoreSearch/SearchResultCell.swift
+++ StoreSearch/SearchResultCell.swift
@@ -13,6 +13,8 @@ class SearchResultCell: UITableViewCell {
   @IBOutlet weak var artistNameLabel: UILabel!
   @IBOutlet weak var artworkImageView: UIImageView!
   
+  var downloadTask: URLSessionDownloadTask?
+  
   override func awakeFromNib() {
     super.awakeFromNib()
     let selectedView = UIView(frame: CGRect.zero)
@@ -27,4 +29,42 @@ class SearchResultCell: UITableViewCell {
     // Configure the view for the selected state
   }
   
+  func configure(for searchResult: SearchResult) {
+    nameLabel.text = searchResult.name
+    
+    if searchResult.artistName.isEmpty {
+      artistNameLabel.text = "Unknown"
+    } else {
+      artistNameLabel.text = String(format: "%@ (%@)",
+                                    searchResult.artistName, kindForDisplay(searchResult.kind))
+    }
+    
+    artworkImageView.image = UIImage(named: "Placeholder")
+    if let smallURL = URL(string: searchResult.artworkSmallURL) {
+      downloadTask = artworkImageView.loadImage(url: smallURL)
+    }
+  }
+  
+  func kindForDisplay(_ kind: String) -> String {
+    switch kind {
+    case "album": return "Album"
+    case "audiobook": return "Audio Book"
+    case "book": return "Book"
+    case "ebook": return "E-Book"
+    case "feature-movie": return "Movie"
+    case "music-video": return "Music Video"
+    case "podcast": return "Podcast"
+    case "software": return "App"
+    case "song": return "Song"
+    case "tv-episode": return "TV Episode"
+    default: return kind
+    }
+  }
+  
+  override func prepareForReuse() {
+    super.prepareForReuse()
+    
+    downloadTask?.cancel()
+    downloadTask = nil
+  }
 }
--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -305,30 +305,8 @@ extension SearchViewController: UITableViewDataSource {
         for: indexPath) as! SearchResultCell
       
       let searchResult = searchResults[indexPath.row]
-      cell.nameLabel.text = searchResult.name
-
-      if searchResult.artistName.isEmpty {
-        cell.artistNameLabel.text = "Unknown"
-      } else {
-        cell.artistNameLabel.text = String(format: "%@ (%@)", searchResult.artistName, kindForDisplay(searchResult.kind))
-      }
+      cell.configure(for: searchResult)
       return cell
-    }
-  }
-
-  func kindForDisplay(_ kind: String) -> String {
-    switch kind {
-    case "album": return "Album"
-    case "audiobook": return "Audio Book"
-    case "book": return "Book"
-    case "ebook": return "E-Book"
-    case "feature-movie": return "Movie"
-    case "music-video": return "Music Video"
-    case "podcast": return "Podcast"
-    case "software": return "App"
-    case "song": return "Song"
-    case "tv-episode": return "TV Episode"
-    default: return kind
     }
   }
 }
