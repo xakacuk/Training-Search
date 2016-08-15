298ac7290e2f886d05e355ae2410359d9f575182
Matthijs <matthijs@Matthijss-MacBook-Pro.local>
2016-08-15 13:32:47 +0200

    Download the artwork and put it on the buttons

--- StoreSearch/LandscapeViewController.swift
+++ StoreSearch/LandscapeViewController.swift
@@ -15,9 +15,13 @@ class LandscapeViewController: UIViewController {
   var searchResults = [SearchResult]()
   
   private var firstTime = true
+  private var downloadTasks = [URLSessionDownloadTask]()
   
   deinit {
     print("deinit \(self)")
+    for task in downloadTasks {
+      task.cancel()
+    }
   }
 
   override func viewDidLoad() {
@@ -100,9 +104,8 @@ class LandscapeViewController: UIViewController {
     var column = 0
     var x = marginX
     for (index, searchResult) in searchResults.enumerated() {
-      let button = UIButton(type: .system)
-      button.backgroundColor = UIColor.white
-      button.setTitle("\(index)", for: .normal)
+      let button = UIButton(type: .custom)
+      button.setBackgroundImage(UIImage(named: "LandscapeButton"), for: .normal)
 
       button.frame = CGRect(
         x: x + paddingHorz,
@@ -110,6 +113,8 @@ class LandscapeViewController: UIViewController {
         width: buttonWidth, height: buttonHeight)
 
       scrollView.addSubview(button)
+
+      downloadImage(for: searchResult, andPlaceOn: button)
 
       row += 1
       if row == rowsPerPage {
@@ -132,6 +137,26 @@ class LandscapeViewController: UIViewController {
     
     pageControl.numberOfPages = numPages
     pageControl.currentPage = 0
+  }
+  
+  private func downloadImage(for searchResult: SearchResult, andPlaceOn button: UIButton) {
+    if let url = URL(string: searchResult.artworkSmallURL) {
+      let downloadTask = URLSession.shared.downloadTask(with: url) {
+        [weak button] url, response, error in
+        
+        if error == nil, let url = url,
+          let data = try? Data(contentsOf: url),
+          let image = UIImage(data: data) {
+          DispatchQueue.main.async {
+            if let button = button {
+              button.setImage(image, for: .normal)
+            }
+          }
+        }
+      }
+      downloadTask.resume()
+      downloadTasks.append(downloadTask)
+    }
   }
   
   @IBAction func pageChanged(_ sender: UIPageControl) {
